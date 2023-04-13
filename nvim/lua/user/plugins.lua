48650425099ffb2e1516ec00534411395d1189fb
Arunas Skirius <arukomp@gmail.com>
2023-04-13 12:43:49 +0300

    added GH Copilot

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -279,6 +279,10 @@ use({
   end,
 })
 
+use({
+  'github/copilot.vim'
+})
+
 -- Automatically set up your configuration after cloning packer.nvim
 -- Put this at the end after all plugins
 if packer_bootstrap then

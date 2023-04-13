a11a67cda06a948ef229ce21eb2404cc37ec7f5d
Arunas Skirius <arukomp@gmail.com>
2023-04-13 09:40:51 +0300

    enable some null-ls configs

--- nvim/lua/user/plugins/lspconfig.lua
+++ nvim/lua/user/plugins/lspconfig.lua
@@ -39,22 +39,22 @@ require('null-ls').setup({
     require('null-ls').builtins.code_actions.gitsigns,
     require('null-ls').builtins.code_actions.proselint,
     require('null-ls').builtins.diagnostics.eslint_d.with({
-      -- condition = function(utils)
-      --   return utils.root_has_file({ '.eslintrc.js' })
-      -- end,
+      condition = function(utils)
+        return utils.root_has_file({ '.eslintrc.js' })
+      end,
     }),
     require('null-ls').builtins.diagnostics.proselint,
     require('null-ls').builtins.diagnostics.gitlint,
     -- require('null-ls').builtins.diagnostics.luacheck.with({
     --   extra_args = { '--config', vim.fn.stdpath('config') .. '/.luacheckrc' },
     -- }),
-    -- require("null-ls").builtins.diagnostics.phpstan,
-    require('null-ls').builtins.diagnostics.solhint,
+    require("null-ls").builtins.diagnostics.phpstan,
+    -- require('null-ls').builtins.diagnostics.solhint,
     require('null-ls').builtins.diagnostics.trail_space.with({ disabled_filetypes = { 'NvimTree' } }),
     require('null-ls').builtins.formatting.eslint_d.with({
-      -- condition = function(utils)
-      --   return utils.root_has_file({ '.eslintrc.js' })
-      -- end,
+      condition = function(utils)
+        return utils.root_has_file({ '.eslintrc.js' })
+      end,
     }),
     require('null-ls').builtins.formatting.prettierd,
     require('null-ls').builtins.formatting.phpcsfixer,
